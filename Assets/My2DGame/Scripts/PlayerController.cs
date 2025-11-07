8989aa3653cdffa069090f70fac1bffe70721fea
DESKTOP-5V3FRMH\M <gidal569@gmail.com>
2025-11-07 18:52:09 +0900

    1. 플레이어 애니메이션

--- Assets/My2DGame/Scripts/PlayerController.cs
+++ Assets/My2DGame/Scripts/PlayerController.cs
@@ -8,11 +8,90 @@ namespace My2DGame
         //이동 속도
         [SerializeField]
         private float walkSpeed = 5f;   //걷는 속도
+        [SerializeField]
+        private float runSpeed = 7f;    //달리는 속도
+        [SerializeField] 
+        private float jumpForce = 7f;  // 점프 힘
+
         //참조
         private Rigidbody2D rb2D;
+        private Animator animator;
         //입력값
         private Vector2 inputMove = Vector2.zero;
+        //반전
+        private bool isFacingRight = true;
+        //걷기
+        private bool isMove = false;
+        private bool isRun = false;
+        private bool isGrounded = false;
 
+        #endregion
+
+        #region Property
+        public bool IsFacingRight
+        {
+            get { return isFacingRight; }
+            set 
+            {
+                //반전 구현
+                if(isFacingRight != value)
+                {
+                    this.transform.localScale *= new Vector2(-1, 1);
+                }
+                isFacingRight = value; 
+            }
+        }
+        public bool IsMove
+        {
+            get { return isMove; }
+            private set 
+            {
+                isMove = value;
+                animator.SetBool(AnimationString.IsMove, value);
+            }
+        }
+
+        public bool IsRun
+        {
+            get { return isRun; }
+            private set
+            {
+                isRun = value;
+                animator.SetBool(AnimationString.IsRun, value);
+            }
+        }
+
+        public bool IsGrounded
+        {
+            get { return isGrounded; }
+            set
+            {
+                isGrounded = value;
+                animator.SetBool(AnimationString.IsGrounded, value);
+            }
+        }
+        //현재 이동 속도 - 읽기 전용
+        public float CurrentMoveSpeed
+        {
+            get 
+            {
+                if (IsMove) //이동가능
+                {
+                    if(IsRun)
+                    {
+                        return runSpeed;
+                    }
+                    else
+                    {
+                        return walkSpeed;
+                    }
+                }
+                else //이동 불가
+                {
+                    return 0f;
+                }
+            }
+        }
         #endregion
 
         #region Unity Event Method
@@ -20,19 +99,59 @@ namespace My2DGame
         {
             //참조
             rb2D = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
         }
         private void FixedUpdate()
         {
             // 물리 이동
-            rb2D.linearVelocity = new Vector2(inputMove.x * walkSpeed, rb2D.linearVelocity.y);
-
+            rb2D.linearVelocity = new Vector2(inputMove.x * CurrentMoveSpeed, rb2D.linearVelocity.y);
         }
         #endregion
 
         #region Cudtom Method
+        //방향 전환 
+        void SetFacingDirection(Vector2 moveInput)
+        {
+            if(moveInput.x > 0f && IsFacingRight == false)  //오른쪽으로 이동 
+            {
+                IsFacingRight = true;
+            }
+            else if (moveInput.x < 0f && IsFacingRight == true) //왼쪽으로 이동
+            {
+                IsFacingRight = false;
+            }
+        }
         public void OnMove(InputAction.CallbackContext context)
         {
             inputMove = context.ReadValue<Vector2>();
+            IsMove = inputMove != Vector2.zero;
+            //방향 전환
+            SetFacingDirection(inputMove);
+        }
+
+        public void OnRun(InputAction.CallbackContext context)
+        {
+            if (context.started)    //버튼을 눌렀을 때 
+            {
+                IsRun = true;
+            }
+            else if (context.canceled)  //버튼을 땔 때
+            {
+                IsRun = false;
+            }
+            //방향 전환
+            SetFacingDirection(inputMove);
+        }
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                animator.SetTrigger("JumpTrigger");
+
+                rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
+
+                IsGrounded = false;
+            }
         }
         #endregion
     }
---
+++ Assets/My2DGame/Scripts/TouchingDirections.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+namespace My2DGame
+{
+    /// <summary>
+    /// 그라운드, 천정, 벽 체크
+    /// </summary>
+    public class TouchingDirections : MonoBehaviour
+    {
+        #region Variables
+        //참조        
+        private CapsuleCollider2D touchingCol; //접촉하는 충돌체
+        private Animator animator;
+
+        //접촉면 범위
+        [SerializeField] private float groundDistance = 0.05f;
+        [SerializeField] private float cellingDistance = 0.05f;
+        [SerializeField] private float wallDistance = 0.1f;
+
+        //접촉 조건
+        [SerializeField]
+        private ContactFilter2D contactFilter;
+
+        //캐스트 결과
+        private RaycastHit2D[] groundHits = new RaycastHit2D[5];
+        private RaycastHit2D[] cellingHits = new RaycastHit2D[5];
+        private RaycastHit2D[] wallHits = new RaycastHit2D[5];
+
+        //
+        [SerializeField] private bool isGround;
+        [SerializeField] private bool isCelling;
+        [SerializeField] private bool isWall;
+        #endregion
+
+        #region Property
+        public bool IsGround
+        {
+            get { return isGround; }
+            private set
+            {
+                isGround = value;
+                animator.SetBool(AnimationString.IsGrounded, value);
+            }
+        }
+
+        public bool IsCelling
+        {
+            get { return isCelling; }
+            private set
+            {
+                isCelling = value;
+                //애니 파라미터 셋팅
+            }
+        }
+
+        public bool IsWall
+        {
+            get { return isWall; }
+            private set
+            {
+                isWall = value;
+                //애니 파라미터 셋팅
+            }
+        }
+
+        //벽체크 할 방향
+        private Vector2 wallCheckDirection => (this.transform.localScale.x > 0f) ? Vector2.right : Vector2.left;
+        #endregion
+
+        #region Unity Event Method
+        private void Awake()
+        {
+            //참조
+            touchingCol = this.GetComponent<CapsuleCollider2D>();
+            animator = this.GetComponent<Animator>();
+        }
+
+        private void FixedUpdate()
+        {
+            IsGround = (touchingCol.Cast(Vector2.down, contactFilter, groundHits, groundDistance) > 0);
+            IsCelling = (touchingCol.Cast(Vector2.up, contactFilter, cellingHits, cellingDistance) > 0);
+            IsWall = (touchingCol.Cast(wallCheckDirection, contactFilter, wallHits, wallDistance) > 0);
+
+        }
+        #endregion
+    }
+}
